aa454f1ab6a7a4ee7892c903d0f430a832635607
Mallikarjun <91137603+arjunmsit@users.noreply.github.com>
2021-12-07 15:53:45 +0000

    Bhanu changes

--- src/main/java/com/united/drools/controller/TestRestController.java
+++ src/main/java/com/united/drools/controller/TestRestController.java
@@ -1,4 +1,15 @@
 package com.united.drools.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestRestController {
+	
+	@RequestMapping(value = "/getname")
+	public String getProducts() { 
+		
+		return "Bhanumurthi Tellapati";
+	}
+
 }
